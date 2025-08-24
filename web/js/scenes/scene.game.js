117d2ee92abf3d39b48d4b9dc0c033aa500caa91
Roman Nix <rnix@yandex.ru>
2025-08-24 18:58:59 +0000

    Add full screen

--- web/js/scenes/scene.game.js
+++ web/js/scenes/scene.game.js
@@ -19,6 +19,8 @@ GameScene.prototype = {
     create: function(game, data) {
         const self = this;
 
+        this.game.scale.startFullScreen();
+
         this.myClientId = data.myClientId;
         this.myNickname = data.myNickname;
         this.opponentNickname = data.opponentNickname;
